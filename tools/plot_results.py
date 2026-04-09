8a1e8e3684175d0a8208b87f8467ad02d1e7bb6c
stijn <s.heldens@esciencecenter.nl>
2026-04-09 09:27:42 +0200

    Update plotting script

--- tools/plot_results.py
+++ tools/plot_results.py
@@ -1,141 +1,231 @@
 import json
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sbn
+import scienceplots
 import json
+import re
 
-sbn.set("paper")
+plt.style.use(["science", "grid"])
+matplotlib.rcParams.update({
+    "text.usetex": True,
+    "axes.grid" : True,
+    "grid.color": ".8"
+})
+
 figsize = (8, 3)
-n = 5
+
+def load_times(filename):
+    with open(filename) as f:
+        content = f.read()
+
+    lw_times = re.findall("Duration longwave solver: ([0-9.]+)", content)
+    sw_times = re.findall("Duration shortwave solver: ([0-9.]+)", content)
+
+    return (
+            np.median([float(x) for x in lw_times]),
+            np.median([float(x) for x in sw_times]),
+    )
+
+def load_json(filename):
+    with open(filename) as f:
+        return json.load(f)
+
+levels = {
+        "double": 0,
+        "float": 1,
+        "A": 2,
+        "B": 3,
+        "C": 4,
+        "D": 5,
+        "E": 6,
+}
+
+times_lumi = {
+        k: load_times(f"results/lumi/output_{v}.txt") for k,v in levels.items()
+}
+
+times_leonardo = {
+        k: load_times(f"results/leonardo/output_{v}.txt") for k,v in levels.items()
+}
+
+errors_lumi = {
+        k: load_json(f"results/lumi/result_{v}.json") for k,v in levels.items()
+}
+
+errors_a100 = {
+        k: load_json(f"results/a100/result_{v}.json") for k,v in levels.items()
+}
+
+labels = "ABCDE"
+n = len(labels)
 xs = np.arange(n)
-
-times = [
-        (2862.284424, 1930.439331),
-        (2355.549316, 1759.989014),
-        (1649.722168, 1105.866699),
-        (1437.307861, 1036.919922),
-        (1406.610474, 937.159607),
-]
-
-labels = ["A", "B", "C", "D", "E"]
-
-def load_datasets():
-    results = [
-        json.load(open(f"result_{i}.json")) for i in range(n)
-    ]
-
-    datasets = dict()
-
-    for a in results[0]:
-        for b in results[0][a]:
-            datasets[a,b] = [results[i][a][b] for i in range(n)]
-
-    return datasets
 
 def savefig(name):
     filename = f"{name}.eps"
     plt.tight_layout()
-    plt.savefig(filename)
+    plt.savefig(filename, dpi=200)
     print(f"saved as {filename}")
 
-results = load_datasets()
 
-def plot_times():
-    plt.figure(figsize=figsize)
+def annotate(label, x, y, ha="left"):
+    plt.annotate(
+            label,
+        (x, y),
+        (x*2 if ha == "left" else x/2, y*0.995),
+        va="center",
+        ha=ha,
+        bbox=dict(
+            boxstyle="round,pad=0.3",
+            fc="white",
+            ec="none",
+            alpha=0.7
+        ),
+        fontsize=10
+    )
+
+def plot_times(name, results, times):
+    plt.figure(figsize=(figsize[0], figsize[1]*0.8))
+
+    baseline = sum(times["double"])
+    time = np.array([sum(times[k]) for k in labels])
+    errors = {k: results[k]["net flux"]["mean_rel_error"] for k in times}
+    errors = {k: results[k]["net flux"]["root_mean_sq_error"] / results[k]["net flux"]["mean"] for k in times}
+
+    print(f"timing results {name}")
+    for label in times:
+        x = errors[label]
+        y = sum(times[label])
+        print(f" * {label}: time={y} speedup={baseline/y} error={x} logerror={np.log10(x)}")
 
     plt.subplot(121)
-    plt.bar(np.arange(n), [p[0] for p in times], label="LW solver")
-    plt.bar(np.arange(n), [p[1] for p in times], bottom=[p[0] for p in times], label="SW solver")
-    plt.ylabel("Execution time (ms)")
-    plt.legend()
+    plt.text(.05, .95, name.capitalize(), ha="left", va="top", transform=plt.gca().transAxes, fontsize=14, backgroundcolor="1")
+
+    plt.bar([-2], [baseline], color=".3")
+    plt.bar([-1], [sum(times["float"])], color=".5")
+
+    for x, y in enumerate(time):
+        plt.bar(x, y)
+
+    xticks = [-2, -1] + list(xs)
+    xlabels = ["FP64", "FP32"] + list(labels)
+    plt.xticks(xticks, xlabels)
+
+    ylim = np.ceil(baseline / 1000.0 + 1) * 1000
     plt.grid(False, axis="x")
-    plt.xticks(xs, labels)
+    plt.xlim(-2.5, len(labels)-.5)
+    plt.ylim(0, ylim)
+    plt.ylabel("Execution time (ms)")
 
     plt.subplot(122)
-    baseline = sum(times[0])
-    plt.bar(np.arange(n), [baseline / sum(p) for p in times])
-    plt.ylabel("Speedup over FP64")
-    plt.xticks(xs, labels)
-    plt.grid(False, axis="x")
+    plt.text(.05, .95, name.capitalize(), ha="left", va="top", transform=plt.gca().transAxes, fontsize=14, backgroundcolor="1")
 
-    savefig("times")
+    for label, t in zip(labels, time):
+        x = errors[label]
+        y = baseline / t
+        plt.scatter(x, y, s=50, marker="x")
 
+        if label != "A":
+            ha="right"
+        else:
+            ha="left"
 
-def plot_rel_error():
-    plt.figure(figsize=figsize)
+        annotate(f"Version {label}", x, y, ha)
 
-    ys = [sum(times[0]) / sum(p) for p in times]
-    xs = results["net flux","mean_rel_error"]
+    x = errors["float"]
+    y = baseline / sum(times["float"])
+    plt.scatter(x, y, s=50, marker=".", c=".5")
+    annotate("Version FP32", x, y, "left")
+
     plt.xscale("log")
-
-    for x, y, label in zip(xs, ys, labels):
-        plt.annotate(
-                f"Version {label}",
-                (x, y),
-                (x*3, y),
-                va="center",
-                ha="left",
-                bbox=dict(
-                    boxstyle="round,pad=0.3",
-                    fc="white",
-                    ec="none",
-                    alpha=0.7
-                ),
-                fontsize=10
-        )
-
-    plt.xlim(np.amin(xs) / 10, np.amax(xs) * 200)
-    plt.ylim(.9, 2.12)
-    plt.scatter(xs, ys, s=100)
-    plt.title("Error on net flux (LW + SW)")
-    plt.xlabel("Mean relative error on net flux")
+    plt.xlabel("Normalized RMSE on net flux (W m$^{-2}$)")
     plt.ylabel("Speedup over FP64")
+    plt.ylim(1.0, 2.3)
+    plt.xlim(10**-9, 1)
+
+    savefig(f"times_{name}")
+
+def plot_heating_rate(results):
+    plt.figure(figsize=(figsize[0], figsize[1]))
+    marker_step = 5 # add marker every 5 points
+    layer_height = 25 # meter per layer
+    nz = 1500 // layer_height
+    heights = np.arange(nz) * layer_height
+    zorder = 100
+
+    markers = ["^", "v", "x", "|", "."]
+    xlim = (-5, 30)
+
+    plt.subplot(131)
+
+    layers = results["double"]["heating rate layers"]
+    ys = [l["mean"] for l in layers][:nz]
+    plt.plot(ys, heights, c=".3", label="FP64")
+
+    plt.legend(loc="upper right")
+    plt.title("Reference output")
+    plt.xlim(*xlim)
+    plt.ylim(0, np.amax(heights))
+    plt.xlabel("Mean heating rate per layer (K/day)")
+    plt.ylabel("Height from surface (meters)")
+
+    plt.subplot(132)
+    ax = plt.gca()
+    #axi = ax.inset_axes([.5, .5, .25, .25])
+    #axi.set_xlim(1000, 1500)
+    #axi.set_ylim(1-eps, 1+eps)
+    #ax.indicate_inset_zoom(axi)
+
+    for key, marker in zip(labels, markers):
+        # Get the data
+        layers = results[key]["heating rate layers"]
+        ys = [l["mean"] + l["mean_bias"] for l in layers][:nz]
+
+        # Get the color
+        [l] = plt.plot([], [], label=key, marker=marker)
+        color = l.get_color()
+
+        # Plot the real results
+        ax.plot(ys, heights, zorder=zorder, color=color)
+        ax.plot(ys[::marker_step], heights[::marker_step], c=color, marker=marker, ls="")
+
+        #axi.plot(heights, ys, zorder=zorder, color=color)
+
+    plt.title("Mixed-precision output")
+    plt.xlim(*xlim)
+    plt.ylim(0, np.amax(heights))
+    plt.xlabel("Mean heating rate per layer (K/day)")
+    plt.legend(loc="upper right").set_zorder(1000)
+    plt.tick_params('y', labelleft=False)
+
+    plt.subplot(133)
+
+    print("heating rate error:")
+
+    for key, marker in zip(labels, markers):
+        # Get the data
+        layers = results[key]["heating rate layers"]
+        ys = [np.abs(l["mean_bias"]) for l in layers][:nz]
+        #ys = [np.abs(l["root_mean_sq_error"]) / np.abs(l["mean"]) for l in layers][:nz]
+        print(f" * {key}: heating rate error=10**{np.log10(np.average(ys))}")
+
+        # Get the color
+        [l] = plt.plot([], [], label=key, marker=marker)
+        color = l.get_color()
+
+        # Plot the real results
+        plt.plot(ys, heights, ys, color=color)
+        plt.plot(ys[::marker_step], heights[::marker_step], c=color, marker=marker, ls="")
+
+    plt.title("Difference reference vs output")
+    plt.xlim(1e-9, 100)
+    plt.ylim(0, np.amax(heights))
     plt.xscale("log")
-    savefig("rel_error")
+    plt.xlabel("Mean heating rate per layer (K/day)")
+    plt.tick_params('y', labelleft=False)
 
+    savefig("heating_rate")
 
-def plot_abs_error():
-    plt.figure(figsize=figsize)
-    for index in range(2):
-        w = .7 / 3
-        key = ["mean_bias", "root_mean_sq_error"][index]
-
-        plt.subplot(121 + index)
-
-        kwargs = dict(s=50, marker="d")
-        plt.scatter(xs - w, np.abs(results["LW flux", key]), label="LW flux", **kwargs)
-        plt.scatter(xs, np.abs(results["SW flux", key]), label="SW flux", **kwargs)
-        plt.scatter(xs + w, np.abs(results["net flux", key]), label="Net flux", **kwargs)
-
-        for x in range(n - 1):
-            plt.axvline(x+.5, c="1")
-
-        plt.title(["Flux, mean bias", "Flux, RMSE"][index])
-        plt.ylabel(["Mean bias (W m⁻²)", "RMSE (W m⁻²)"][index])
-        plt.yscale("log")
-        plt.legend()
-        plt.grid(False, axis="x")
-        plt.xticks(xs, labels)
-
-    savefig("abs_error")
-
-def plot_heating_error():
-    plt.figure(figsize=figsize)
-    plt.suptitle("Error on heating rate at surface and top of atmosphere (TOA)")
-
-    for index in [0, 1]:
-        plt.subplot(121 + index)
-        w = .4
-        plt.bar(xs - .5*w, np.abs(results["heating rate (surface)","mean_abs_error"]), w, label="Surface")
-        plt.bar(xs + .5*w, np.abs(results["heating rate (TOA)","mean_abs_error"]), w, label="TOA")
-        plt.ylabel("Mean error of heating rate (K/day)")
-        plt.legend()
-        plt.ylim(0, [0.005, 5][index])
-        plt.xticks(xs, labels)
-
-    savefig("heating_error")
-
-#plot_times()
-#plot_rel_error()
-#plot_abs_error()
-plot_heating_error()
+plot_times("leonardo", errors_a100, times_leonardo)
+plot_times("lumi", errors_lumi, times_lumi)
+plot_heating_rate(errors_a100)

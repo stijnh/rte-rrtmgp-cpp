16462d74b37db63f38f6dc2351e87c9eb18d5e4e
stijn <s.heldens@esciencecenter.nl>
2026-05-08 14:34:24 +0200

    Add results on energy efficiency

--- tools/plot_results.py
+++ tools/plot_results.py
@@ -15,6 +15,23 @@ matplotlib.rcParams.update({
 
 figsize = (8, 3)
 
+def load_energy(filename):
+    with open(filename) as f:
+        content = f.read()
+
+    lw_matches = re.findall(r"Duration longwave solver: ([0-9.]+) \(ms\), ([0-9.]+) J", content)
+    sw_matches = re.findall(r"Duration shortwave solver: ([0-9.]+) \(ms\), ([0-9.]+) J", content)
+
+    lw_eff = [float(e)/float(t) for t,e in lw_matches]
+    sw_eff = [float(e)/float(t) for t,e in sw_matches]
+
+    lw_times, sw_times = load_times(filename)
+
+    return (
+            np.median(lw_eff) * lw_times,
+            np.median(sw_eff) * sw_times,
+    )
+
 def load_times(filename):
     with open(filename) as f:
         content = f.read()
@@ -41,28 +58,12 @@ levels = {
         "E": 6,
 }
 
-times_lumi = {
-        k: load_times(f"results/lumi/output_{v}.txt") for k,v in levels.items()
-}
-
-times_leonardo = {
-        k: load_times(f"results/leonardo/output_{v}.txt") for k,v in levels.items()
-}
-
-errors_lumi = {
-        k: load_json(f"results/lumi/result_{v}.json") for k,v in levels.items()
-}
-
-errors_a100 = {
-        k: load_json(f"results/a100/result_{v}.json") for k,v in levels.items()
-}
-
 labels = "ABCDE"
 n = len(labels)
 xs = np.arange(n)
 
 def savefig(name):
-    filename = f"{name}.eps"
+    filename = f"{name}.png"
     plt.tight_layout()
     plt.savefig(filename, dpi=200)
     print(f"saved as {filename}")
@@ -84,11 +85,14 @@ def annotate(label, x, y, ha="left"):
         fontsize=10
     )
 
-def plot_times(name, results, times):
+def plot_performance(name, results, times, unit="time"):
+    assert unit in ("time", "energy")
     plt.figure(figsize=(figsize[0], figsize[1]*0.8))
 
     baseline = sum(times["double"])
-    time = np.array([sum(times[k]) for k in labels])
+    time = {k: sum(times[k]) for k in times}
+    speedup = {k: baseline / t for k, t in time.items()}
+
     errors = {k: results[k]["net flux"]["mean_rel_error"] for k in times}
     errors = {k: results[k]["net flux"]["root_mean_sq_error"] / results[k]["net flux"]["mean"] for k in times}
 
@@ -104,25 +108,34 @@ def plot_times(name, results, times):
     plt.bar([-2], [baseline], color=".3")
     plt.bar([-1], [sum(times["float"])], color=".5")
 
-    for x, y in enumerate(time):
-        plt.bar(x, y)
+    for x, l in enumerate(labels):
+        plt.bar(x, time[l])
 
     xticks = [-2, -1] + list(xs)
     xlabels = ["FP64", "FP32"] + list(labels)
     plt.xticks(xticks, xlabels)
 
-    ylim = np.ceil(baseline / 1000.0 + 1) * 1000
+    if unit == "energy":
+        ymultiple = 250
+    else:
+        ymultiple = 1000
+
+    ylim = np.ceil(baseline / ymultiple + 1) * ymultiple
     plt.grid(False, axis="x")
     plt.xlim(-2.5, len(labels)-.5)
     plt.ylim(0, ylim)
-    plt.ylabel("Execution time (ms)")
+
+    if unit == "energy":
+        plt.ylabel("Energy usage (J)")
+    else:
+        plt.ylabel("Execution time (ms)")
 
     plt.subplot(122)
     plt.text(.05, .95, name.capitalize(), ha="left", va="top", transform=plt.gca().transAxes, fontsize=14, backgroundcolor="1")
 
-    for label, t in zip(labels, time):
+    for label in labels:
         x = errors[label]
-        y = baseline / t
+        y = speedup[label]
         plt.scatter(x, y, s=50, marker="x")
 
         if label != "A":
@@ -133,17 +146,24 @@ def plot_times(name, results, times):
         annotate(f"Version {label}", x, y, ha)
 
     x = errors["float"]
-    y = baseline / sum(times["float"])
+    y = speedup["float"]
     plt.scatter(x, y, s=50, marker=".", c=".5")
     annotate("Version FP32", x, y, "left")
 
     plt.xscale("log")
     plt.xlabel("Normalized RMSE on net flux (W m$^{-2}$)")
-    plt.ylabel("Speedup over FP64")
-    plt.ylim(1.0, 2.3)
     plt.xlim(10**-9, 1)
+    plt.ylim(1.0, 2.3)
 
-    savefig(f"times_{name}")
+    if unit == "energy":
+        plt.ylabel("Energy efficiency vs FP64")
+    else:
+        plt.ylabel("Speedup over FP64")
+
+    if unit == "energy":
+        savefig(f"energy_{name}")
+    else:
+        savefig(f"times_{name}")
 
 def plot_heating_rate(results):
     plt.figure(figsize=(figsize[0], figsize[1]))
@@ -226,6 +246,72 @@ def plot_heating_rate(results):
 
     savefig("heating_rate")
 
-plot_times("leonardo", errors_a100, times_leonardo)
-plot_times("lumi", errors_lumi, times_lumi)
-plot_heating_rate(errors_a100)
+def plot_energy_efficiency(name, times, energy):
+    plt.figure(figsize=figsize)
+    colors = {"double": ".3", "float": ".5"}
+
+    plt.subplot(121)
+    xs = [sum(times[l]) for l in labels]
+    ys = [sum(energy[l]) for l in labels]
+
+    for x, y in zip(xs, ys):
+        plt.scatter([x], [y], marker="x")
+
+    tdp = 300
+    xlim = (1500, 3500)
+    ylim = tuple(np.array(xlim) * tdp * 1e-3)
+    plt.plot(xlim, ylim, c=".1")
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+    plt.xlabel("Execution time (ms)")
+    plt.ylabel("Energy usage (J)")
+    plt.text(
+            xlim[0] + (xlim[1]-xlim[0])*0.48,
+            ylim[0] + (ylim[1]-ylim[0])*0.52,
+            "TDP 300W",
+            va="center", ha="center", rotation=35)
+
+    plt.subplot(122)
+    xticks = ["double", "float"] + list(labels)
+    for i, l in enumerate(xticks):
+        plt.bar(i, sum(energy[l]) / sum(times[l]) * 1000, color=colors.get(l))
+
+    plt.xticks(range(len(xticks)), xticks)
+    plt.ylim(0, 350)
+    plt.text(-.5, tdp * 1.02, "TDP 300W", ha="left", va="bottom", backgroundcolor="1")
+    plt.axhline(tdp, c=".1", zorder=100)
+    plt.ylabel("Power (W)")
+    savefig(f"efficiency_{name}")
+
+
+times_lumi = {
+        k: load_times(f"results/lumi/output_{v}.txt") for k,v in levels.items()
+}
+
+times_leonardo = {
+        k: load_times(f"results/leonardo/output_{v}.txt") for k,v in levels.items()
+}
+
+times_snellius = {
+        k: load_times(f"results/a100/output_{v}.txt") for k,v in levels.items()
+}
+
+energy_snellius = {
+        k: load_energy(f"results/a100/output_{v}.txt") for k,v in levels.items()
+}
+
+errors_lumi = {
+        k: load_json(f"results/lumi/result_{v}.json") for k,v in levels.items()
+}
+
+errors_a100 = {
+        k: load_json(f"results/a100/result_{v}.json") for k,v in levels.items()
+}
+
+
+plot_performance("snellius", errors_a100, energy_snellius, unit="energy")
+plot_performance("snellius", errors_a100, times_snellius, unit="time")
+plot_energy_efficiency("snellius", times_snellius, energy_snellius)
+#plot_performance("leonardo", errors_a100, times_leonardo)
+#plot_performance("lumi", errors_lumi, times_lumi)
+#plot_heating_rate(errors_a100)
